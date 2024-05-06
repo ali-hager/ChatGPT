c17543b9ffe74dfaf93de5b5e8f96aa6218d5511
Ali Hager <ahager2013@gmail.com>
2024-05-06 16:18:38 -0400

    frontend

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 import { config } from "dotenv";
-config();
-
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import OpenAI from "openai";
+import readline from "readline";
+
+config();
 
 const app = express();
 const port = 3000;
+const openai = new OpenAI({
+  organization: "org-zLQRXWN471215HzDCc7JXJRs",
+  apiKey: process.env.API_KEY,
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/", async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Welcome to my API!");
+});
 
-  const { message } = req.body;
+app.post("/", async (req, res) => {
+  const { messages } = req.body;
+  console.log(messages);
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: `${message}` }],
+    messages: [
+      {
+        role: "system",
+        content: "You are DesignGPT helpful assistant graphics design chatbot.",
+      },
+      ...messages,
+    ],
     model: "gpt-3.5-turbo",
   });
   res.json({
     completion: completion.choices[0].message,
   });
-});
-
-import OpenAI from "openai";
-import readline from "readline";
-
-const openai = new OpenAI({
-  organization: "org-zLQRXWN471215HzDCc7JXJRs",
-  apiKey: process.env.API_KEY,
 });
 
 const userInterface = readline.createInterface({
@@ -37,15 +46,14 @@ const userInterface = readline.createInterface({
 });
 
 userInterface.prompt();
-userInterface.on("line", async input => {
-  const res = await openai.chat.completions
-  .create({
+userInterface.on("line", async (input) => {
+  const res = await openai.chat.completions.create({
     messages: [{ role: "user", content: input }],
     model: "gpt-3.5-turbo",
-  })
+  });
   console.log(res.choices[0].message.content);
-  userInterface.prompt()
-})
+  userInterface.prompt();
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
